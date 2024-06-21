095ae8d1dfc22a02f3c8e3751441f717d743a184
JaewonChoi <wefhio1985@gmail.com>
2024-06-21 19:43:17 +0900

    chore: 스프링 프로젝트 생성

---
+++ bookforest/src/main/java/com/gwangju3/bookforest/BookforestApplication.java
@@ -0,0 +1,13 @@
+package com.gwangju3.bookforest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookforestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookforestApplication.class, args);
+	}
+
+}
---
+++ bookforest/src/test/java/com/gwangju3/bookforest/BookforestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gwangju3.bookforest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookforestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
