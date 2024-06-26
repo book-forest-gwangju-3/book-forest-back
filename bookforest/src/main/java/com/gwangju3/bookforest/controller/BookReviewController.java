9f8082602fb064152495beadc7b5438cd39a691b
JaewonChoi <wefhio1985@gmail.com>
2024-06-26 15:25:25 +0900

    feat: 독후감 상세 기본 조회

--- bookforest/src/main/java/com/gwangju3/bookforest/controller/BookReviewController.java
+++ bookforest/src/main/java/com/gwangju3/bookforest/controller/BookReviewController.java
@@ -2,9 +2,11 @@ package com.gwangju3.bookforest.controller;
 
 import com.gwangju3.bookforest.domain.BookReview;
 import com.gwangju3.bookforest.dto.bookreview.ReadBookReviewListResponse;
+import com.gwangju3.bookforest.dto.bookreview.ReadBookReviewResponse;
 import com.gwangju3.bookforest.service.BookReviewService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,6 +21,12 @@ public class BookReviewController {
     public ReadBookReviewListResponse bookReviews() {
         List<BookReview> bookReviews = bookReviewService.findAll();
         return new ReadBookReviewListResponse(bookReviews);
+    }
+
+    @GetMapping("/book-reviews/{book_review_id}")
+    public ReadBookReviewResponse bookReview(@PathVariable("book_review_id") Long id) {
+        BookReview bookReview = bookReviewService.findBook(id);
+        return new ReadBookReviewResponse(bookReview);
     }
 }
 
